ee9a8c7a86660b2d42693a6d94d5093de7dc4b5c
camdentadhg <camdent@gmail.com>
2024-06-14 20:29:11 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+}
+
+export default App
+
+//build magic eight ball component
+//style magic eight ball component
+
+// add a reset button
+// display counts of # of times the ball shows up as each of the three different colors
+// create color boxes game
+// set up default properties
+// feedback on changed box
